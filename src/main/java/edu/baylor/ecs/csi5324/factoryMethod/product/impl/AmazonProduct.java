c542092fd54878861ad6069e550c271132196ed1
arod40 <arodacademic@gmail.com>
2023-02-25 22:54:23 -0600

    6. Add AmazonProduct that acts as an adapter and reduces the price by 10%

---
+++ src/main/java/edu/baylor/ecs/csi5324/factoryMethod/product/impl/AmazonProduct.java
@@ -0,0 +1,20 @@
+package edu.baylor.ecs.csi5324.factoryMethod.product.impl;
+
+import edu.baylor.ecs.csi5324.factoryMethod.product.Product;
+
+import java.math.BigDecimal;
+
+public class AmazonProduct  extends Product {
+  public AmazonProduct(String name) {
+    super(name);
+  }
+  public static AmazonProduct make(String name) {
+    return new AmazonProduct(name);
+  }
+
+  public AmazonProduct init(String describtion, BigDecimal price) {
+    super.init(describtion, price);
+    this.setPrice(price.multiply(new BigDecimal("0.1")));
+    return this;
+  }
+}
